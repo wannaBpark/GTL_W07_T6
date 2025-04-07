8bb82b2b0ba11b90194e7f7ace49e7247d990e87
gudtldn <gudtldn1234@gmail.com>
2025-04-07 17:03:04 +0900

    feat: AActor 클래스에 Duplicate 메서드 구현 및 컴포넌트 복제 로직 추가

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/GameFramework/Actor.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/GameFramework/Actor.cpp
@@ -1,10 +1,65 @@
 #include "Actor.h"
-
 #include "World/World.h"
+
 
 UObject* AActor::Duplicate()
 {
-    return Super::Duplicate();
+    ThisClass* NewActor = Cast<ThisClass>(Super::Duplicate());
+
+    NewActor->Owner = Owner;
+
+    // 기본적으로 있던 컴포넌트 제거
+    TSet CopiedComponents = NewActor->OwnedComponents;
+    for (UActorComponent* Components : CopiedComponents)
+    {
+        Components->DestroyComponent();
+    }
+    NewActor->OwnedComponents.Empty();
+
+
+    // 부모-자식 관계 저장용 맵
+    TMap<USceneComponent*, USceneComponent*> ParentChildMap;
+
+    // 컴포넌트 복제 및 부모-자식 관계 추적
+    TMap<USceneComponent*, USceneComponent*> OriginalToDuplicateMap; // 원본 -> 복제된 컴포넌트 매핑
+
+    for (UActorComponent* Component : OwnedComponents)
+    {
+        UActorComponent* NewComponent = Cast<UActorComponent>(Component->Duplicate());
+        NewComponent->OwnerPrivate = NewActor;
+        NewActor->OwnedComponents.Add(NewComponent);
+
+        // RootComponent 설정
+        if (RootComponent == Component)
+        {
+            NewActor->RootComponent = static_cast<USceneComponent*>(NewComponent);
+        }
+
+        // 부모-자식 관계 저장 (USceneComponent만 해당)
+        if (USceneComponent* SceneComp = Cast<USceneComponent>(Component))
+        {
+            OriginalToDuplicateMap.Add(SceneComp, static_cast<USceneComponent*>(NewComponent));
+            if (USceneComponent* ParentComp = SceneComp->GetAttachParent())
+            {
+                ParentChildMap.Add(static_cast<USceneComponent*>(NewComponent), ParentComp);
+            }
+        }
+
+        // 컴포넌트 초기화
+        NewComponent->InitializeComponent();
+    }
+
+    // 부모-자식 관계 복원
+    for (const auto& [ChildComp, OriginalParentComp] : ParentChildMap)
+    {
+        // 복제된 부모를 찾아 설정
+        if (USceneComponent** NewParentComp = OriginalToDuplicateMap.Find(OriginalParentComp))
+        {
+            ChildComp->AttachToComponent(*NewParentComp);
+        }
+    }
+
+    return NewActor;
 }
 
 void AActor::BeginPlay()
@@ -104,7 +159,10 @@ bool AActor::SetRootComponent(USceneComponent* NewRootComponent)
             USceneComponent* OldRootComponent = RootComponent;
             RootComponent = NewRootComponent;
 
-            OldRootComponent->SetupAttachment(RootComponent);
+            if (OldRootComponent)
+            {
+                OldRootComponent->SetupAttachment(RootComponent);
+            }
         }
         return true;
     }
@@ -115,7 +173,7 @@ bool AActor::SetActorLocation(const FVector& NewLocation)
 {
     if (RootComponent)
     {
-        RootComponent->SetLocation(NewLocation);
+       RootComponent->SetLocation(NewLocation);
         return true;
     }
     return false;
