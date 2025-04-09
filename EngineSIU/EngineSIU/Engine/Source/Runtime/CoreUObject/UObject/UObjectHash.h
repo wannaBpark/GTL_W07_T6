84279823cbcf64831ebe0a2360f67f9b5c4bc493
Joochan <joochan123123@gmail.com>
2025-04-10 04:35:14 +0900

    TODO추가.

--- EngineSIU/EngineSIU/Engine/Source/Runtime/CoreUObject/UObject/UObjectHash.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/CoreUObject/UObject/UObjectHash.h
@@ -22,5 +22,6 @@ void RemoveFromClassMap(UObject* Object);
  * ClassToLookFor와 일치하는 자식 UClass를 반환합니다.
  * @param ClassToLookFor 찾을 자식클래스의 부모 클래스
  * @param Results ClassToLookFor의 파생클래스가 담길 목록
+ * TODO: GetClassMap 사용해서 Spawn 안 된 UClass도 찾기
  */
 void GetChildOfClass(UClass* ClassToLookFor, TArray<UClass*>& Results);
