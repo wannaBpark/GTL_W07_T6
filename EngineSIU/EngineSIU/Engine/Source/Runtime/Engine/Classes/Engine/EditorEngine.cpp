976c8d2d932364c85db3c33ccf7d20e7dfe89f81
OhJeongHun <45549589+ojh6507@users.noreply.github.com>
2025-04-09 14:25:46 +0900

    fix: pie모드에서 picking Object 해제

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Engine/EditorEngine.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Engine/EditorEngine.cpp
@@ -106,6 +106,10 @@ void UEditorEngine::EndPIE()
         PIEWorld->Release();
         GUObjectArray.MarkRemoveObject(PIEWorld);
         PIEWorld = nullptr;
+
+        // TODO: PIE에서 EditorWorld로 돌아올 때, 기존 선택된 Picking이 유지되어야 함. 현재는 에러를 막기위해 임시조치.
+        DeselectActor(GetSelectedActor());
+        DeselectComponent(GetSelectedComponent());
     }
     // 다시 EditorWorld로 돌아옴.
     ActiveWorld = EditorWorld;
