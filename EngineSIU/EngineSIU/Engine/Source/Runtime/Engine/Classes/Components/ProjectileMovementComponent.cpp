b66926033137c966833f7d6e02fb2390138db867
OhJeongHun <45549589+ojh6507@users.noreply.github.com>
2025-04-09 14:00:41 +0900

    chore: 생명주기 destroy 주석 처리

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Components/ProjectileMovementComponent.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Components/ProjectileMovementComponent.cpp
@@ -6,7 +6,7 @@ UProjectileMovementComponent::UProjectileMovementComponent()
     InitialSpeed = 10.0f;
     MaxSpeed = 2000.0f;
     ProjectileGravityScale = 1.0f;
-    Velocity = FVector(10.f,0,0);
+    Velocity = FVector(10.f, 0, 0);
 }
 
 UProjectileMovementComponent::~UProjectileMovementComponent()
@@ -36,15 +36,21 @@ void UProjectileMovementComponent::TickComponent(float dt)
         FVector NewLocation = GetOwner()->GetRootComponent()->GetWorldLocation() + Velocity * DeltaTime;
         GetOwner()->GetRootComponent()->SetRelativeLocation(NewLocation);
     }
-}
 
-    AccumulatedTime += DeltaTime;
-    
+    /*
+    * TODO: Picking 됐을 때 Destroy 호출시
+    * 
+    *  USceneComponent::GetWorldLocation() const
+	if (AttachParent)  예외 발생함.
+    */
+
+   /* 
+      AccumulatedTime += DeltaTime;
     if (AccumulatedTime >= ProjectileLifetime)
-    {
-        if (GetOwner())
-        {
-            GetOwner()->Destroy();
-        }
-    }
+      {
+          if (GetOwner())
+          {
+              GetOwner()->Destroy();
+          }
+      }*/
 }
