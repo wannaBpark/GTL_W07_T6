9e4f6fefe932c27b9f328a1b445fdfdf42964ec5
gudtldn <gudtldn1234@gmail.com>
2025-04-10 04:47:52 +0900

    wip: FSlateAppMessageHandler 클래스에 키 및 마우스 이벤트 처리 기능 추가

--- EngineSIU/EngineSIU/Engine/Source/Editor/LevelEditor/SlateAppMessageHandler.h
+++ EngineSIU/EngineSIU/Engine/Source/Editor/LevelEditor/SlateAppMessageHandler.h
@@ -1,7 +1,57 @@
 ﻿#pragma once
+#include "HAL/PlatformType.h"
 
-class SlateAppMessageHandler
+
+struct FVector2D;
+
+// TODO: 나중에 적당한 파일로 옮기기
+namespace EMouseButtons
+{
+enum Type : uint8
+{
+    Left = 0,
+    Middle,
+    Right,
+    Thumb01,
+    Thumb02,
+
+    Invalid,
+};
+}
+
+class FSlateAppMessageHandler
 {
 public:
-    
+    FSlateAppMessageHandler();
+
+    void ProcessMessage(HWND hWnd, uint32 Msg, WPARAM wParam, LPARAM lParam);
+
+public:
+    void OnKeyChar(const TCHAR Character, const bool IsRepeat) const;
+    void OnKeyDown(const int32 KeyCode, const uint32 CharacterCode, const bool IsRepeat) const;
+    void OnKeyUp(const int32 KeyCode, const uint32 CharacterCode, const bool IsRepeat) const;
+    void OnMouseDown(const EMouseButtons::Type Button, const FVector2D CursorPos) const;
+    void OnMouseUp(const EMouseButtons::Type Button, const FVector2D CursorPos) const;
+    void OnMouseDoubleClick(const EMouseButtons::Type Button, const FVector2D CursorPos) const;
+    void OnMouseWheel(const float Delta, const FVector2D CursorPos) const;
+    void OnMouseMove();
+    void OnRawMouseMove(const int32 X, const int32 Y);
+
+private:
+    struct EModifierKey
+    {
+        enum Type : uint8
+        {
+            LeftShift,    // VK_LSHIFT
+            RightShift,   // VK_RSHIFT
+            LeftControl,  // VK_LCONTROL
+            RightControl, // VK_RCONTROL
+            LeftAlt,      // VK_LMENU
+            RightAlt,     // VK_RMENU
+            CapsLock,     // VK_CAPITAL
+            Count,
+        };
+    };
+
+    bool ModifierKeyState[EModifierKey::Count];
 };
