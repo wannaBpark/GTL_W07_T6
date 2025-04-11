5d73eb4ed214a61b7ab22bbadf0402bfa938f0f6
gudtldn <gudtldn1234@gmail.com>
2025-04-11 12:52:31 +0900

    feat: FVector2D에 ZeroVector 및 OneVector 상수 추가

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Core/Math/Vector.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Core/Math/Vector.h
@@ -13,6 +13,9 @@ struct FVector2D
 	FVector2D(float InX, float InY) : X(InX), Y(InY) {}
     FVector2D(float Scalar) : X(Scalar), Y(Scalar) {}
 
+    static const FVector2D ZeroVector;
+    static const FVector2D OneVector;
+
 public:
     FVector2D operator+(const FVector2D& Rhs) const
     {
