7035837bf90129ea82e13844cd644433fe7a99ea
gudtldn <gudtldn1234@gmail.com>
2025-04-11 16:30:14 +0900

    feat: WindowsCursor 클래스 추가 및 커서 위치 가져오기/설정 기능 구현

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Windows/WindowsCursor.cpp
@@ -0,0 +1,18 @@
+﻿#include "WindowsCursor.h"
+
+
+FVector2D FWindowsCursor::GetPosition()
+{
+    POINT CursorPos;
+    ::GetCursorPos(&CursorPos);
+
+    return {
+        static_cast<float>(CursorPos.x),
+        static_cast<float>(CursorPos.y)
+    };
+}
+
+void FWindowsCursor::SetPosition(const int32 X, const int32 Y)
+{
+    ::SetCursorPos(X, Y);
+}
---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Windows/WindowsCursor.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "HAL/PlatformType.h"
+#include "Math/Vector.h"
+
+
+/**
+ * WinAPI에서 Cursor관련 로직을 래핑한 클래스 입니다. 
+ */
+struct FWindowsCursor
+{
+    /** 현재 Cursor의 위치를 가져옵니다. */
+    static FVector2D GetPosition();
+
+    /** Cursor의 위치를 설정합니다. */
+    static void SetPosition(const int32 X, const int32 Y);
+};
