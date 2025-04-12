2bc341d480753fbd2e73b33f636c7060f4777b10
gudtldn <gudtldn1234@gmail.com>
2025-04-12 16:52:12 +0900

    feat: 커서 타입을 정의하고 커서 모양을 가져오고 설정하는 기능 추가

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Windows/WindowsCursor.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Windows/WindowsCursor.cpp
@@ -16,3 +16,90 @@ void FWindowsCursor::SetPosition(const int32 X, const int32 Y)
 {
     ::SetCursorPos(X, Y);
 }
+
+ECursorType FWindowsCursor::GetMouseCursor()
+{
+    const HCURSOR Cursor = ::GetCursor();
+    if (Cursor == nullptr)
+    {
+        return ECursorType::None;
+    }
+
+    // 시스템 커서 핸들과 비교
+    if (Cursor == LoadCursor(nullptr, IDC_ARROW))
+    {
+        return ECursorType::Arrow;
+    }
+    else if (Cursor == LoadCursor(nullptr, IDC_IBEAM))
+    {
+        return ECursorType::TextEditBeam;
+    }
+    else if (Cursor == LoadCursor(nullptr, IDC_CROSS))
+    {
+        return ECursorType::Crosshairs;
+    }
+    else if (Cursor == LoadCursor(nullptr, IDC_HAND))
+    {
+        return ECursorType::Hand;
+    }
+    else if (Cursor == LoadCursor(nullptr, IDC_SIZEWE))
+    {
+        return ECursorType::ResizeLeftRight;
+    }
+    else if (Cursor == LoadCursor(nullptr, IDC_SIZENS))
+    {
+        return ECursorType::ResizeUpDown;
+    }
+    else if (Cursor == LoadCursor(nullptr, IDC_SIZENWSE))
+    {
+        return ECursorType::ResizeSouthEast; // 실제 WinAPI는 NW-SE 방향
+    }
+    else if (Cursor == LoadCursor(nullptr, IDC_SIZENESW))
+    {
+        return ECursorType::ResizeSouthWest; // 실제 WinAPI는 NE-SW 방향
+    }
+
+    return ECursorType::None;
+}
+
+
+void FWindowsCursor::SetMouseCursor(const ECursorType CursorType)
+{
+    HCURSOR NewCursor;
+
+    // 커서 타입별 핸들 매핑
+    switch (CursorType)  // NOLINT(clang-diagnostic-switch-enum)
+    {
+    case ECursorType::Arrow:
+        NewCursor = LoadCursor(nullptr, IDC_ARROW);
+        break;
+    case ECursorType::TextEditBeam:
+        NewCursor = LoadCursor(nullptr, IDC_IBEAM);
+        break;
+    case ECursorType::Crosshairs:
+        NewCursor = LoadCursor(nullptr, IDC_CROSS);
+        break;
+    case ECursorType::Hand:
+        NewCursor = LoadCursor(nullptr, IDC_HAND);
+        break;
+    case ECursorType::ResizeLeftRight:
+        NewCursor = LoadCursor(nullptr, IDC_SIZEWE);
+        break;
+    case ECursorType::ResizeUpDown:
+        NewCursor = LoadCursor(nullptr, IDC_SIZENS);
+        break;
+    case ECursorType::ResizeSouthEast:
+        NewCursor = LoadCursor(nullptr, IDC_SIZENWSE); // 실제 WinAPI는 NW-SE 방향
+        break;
+    case ECursorType::ResizeSouthWest:
+        NewCursor = LoadCursor(nullptr, IDC_SIZENESW); // 실제 WinAPI는 NE-SW 방향
+        break;
+    case ECursorType::None:
+    default:
+        NewCursor = nullptr; // 커서 숨김
+        break;
+    }
+
+    ::SetCursor(NewCursor);
+}
+
--- EngineSIU/EngineSIU/Engine/Source/Runtime/Windows/WindowsCursor.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Windows/WindowsCursor.h
@@ -3,6 +3,20 @@
 #include "Math/Vector.h"
 
 
+enum class ECursorType : uint8
+{
+    None = 0,
+    Arrow,
+    TextEditBeam,
+    Crosshairs,
+    Hand,
+    ResizeLeftRight,
+    ResizeUpDown,
+    ResizeSouthEast,
+    ResizeSouthWest,
+    Max
+};
+
 /**
  * WinAPI에서 Cursor관련 로직을 래핑한 클래스 입니다. 
  */
@@ -13,4 +27,10 @@ struct FWindowsCursor
 
     /** Cursor의 위치를 설정합니다. */
     static void SetPosition(const int32 X, const int32 Y);
+
+    /** Cursor의 모양을 가져옵니다. */
+    static ECursorType GetMouseCursor();
+
+    /** Cursor의 모양을 설정합니다. */
+    static void SetMouseCursor(const ECursorType CursorType = ECursorType::Arrow);
 };
