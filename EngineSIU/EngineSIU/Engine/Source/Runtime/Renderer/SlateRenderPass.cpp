8f662e76ba15b9fed4a451f78e1e6d97d08c4a6f
Mstone8370 <mstone8370@gmail.com>
2025-04-14 16:38:31 +0900

    feat: 리니어 샘플러가 더 나은듯

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Renderer/SlateRenderPass.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Renderer/SlateRenderPass.cpp
@@ -32,7 +32,7 @@ void FSlateRenderPass::Initialize(FDXDBufferManager* InBufferManager, FGraphicsD
     }
 
     D3D11_SAMPLER_DESC SamplerDesc = {};
-    SamplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
+    SamplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
     SamplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_CLAMP;
     SamplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP;
     SamplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
